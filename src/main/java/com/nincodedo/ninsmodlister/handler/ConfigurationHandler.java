8f9641425566c74270bf3036618c3a841fe0124f
Nincodedo <nincodedo@gmail.com>
2015-06-23 20:06:06 -0500

    Added version replace in reference
    Closes #3

--- src/main/java/com/nincodedo/ninsmodlister/handler/ConfigurationHandler.java
+++ src/main/java/com/nincodedo/ninsmodlister/handler/ConfigurationHandler.java
@@ -5,6 +5,7 @@ import java.io.File;
 import net.minecraftforge.common.config.Configuration;
 
 import com.nincodedo.ninsmodlister.NinsModLister;
+import com.nincodedo.ninsmodlister.reference.Reference;
 
 import cpw.mods.fml.client.event.ConfigChangedEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -33,7 +34,7 @@ public class ConfigurationHandler {
 	@SubscribeEvent
 	public void onConfigurationChangedEvent(
 			ConfigChangedEvent.OnConfigChangedEvent event) {
-		if (event.modID.equalsIgnoreCase(NinsModLister.MOD_ID)) {
+		if (event.modID.equalsIgnoreCase(Reference.MOD_ID)) {
 			loadConfiguration();
 		}
 	}
